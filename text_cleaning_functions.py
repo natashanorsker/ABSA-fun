2555cc917ea36edca57944ee6d1a6437ac18b58c
natashanorsker <natashanorsker@gmail.com>
2022-02-10 18:53:46 +0100

    add current stuff to gt

---
+++ text_cleaning_functions.py
@@ -0,0 +1,246 @@
+import re
+import unidecode
+
+# a lot of theses functions come directly from: https://towardsdatascience.com/cleaning-preprocessing-text-data-by-building-nlp-pipeline-853148add68a
+
+def remove_whitespace(text):
+    pattern = re.compile(r'\s+') 
+    Without_whitespace = re.sub(pattern, ' ', text)
+    # There are some instances where there is no space after '?' & ')', 
+    # So I am replacing these with one space so that It will not consider two words as one token.
+    text = Without_whitespace.replace('?', ' ? ').replace(')', ') ')
+    return text
+
+def accented_characters_removal(text):
+    text = unidecode.unidecode(text)
+    return text
+
+
+def lower_casing_text(text):
+    # Convert text to lower case
+    text = text.lower()
+    return text
+
+def reducing_incorrect_character_repeatation(text):
+    """
+    This Function will reduce repeatition to two characters 
+    for alphabets and to one character for punctuations.
+    
+    arguments:
+         input_text: "text" of type "String".
+         
+    return:
+        value: Finally formatted text with alphabets repeating to 
+        two characters & punctuations limited to one repeatition 
+        
+    Example:
+    Input : Realllllllllyyyyy,        Greeeeaaaatttt   !!!!?....;;;;:)
+    Output : Reallyy, Greeaatt !?.;:)
+    
+    """
+    # Pattern matching for all case alphabets
+    Pattern_alpha = re.compile(r"([A-Za-z])\1{1,}", re.DOTALL)
+    
+    # Limiting all the  repeatation to two characters.
+    Formatted_text = Pattern_alpha.sub(r"\1\1", text) 
+    
+    # Pattern matching for all the punctuations that can occur
+    Pattern_Punct = re.compile(r'([.,/#!$%^&*?;:{}=_`~()+-])\1{1,}')
+    
+    # Limiting punctuations in previously formatted string to only one.
+    Combined_Formatted = Pattern_Punct.sub(r'\1', Formatted_text)
+    
+    # The below statement is replacing repeatation of spaces that occur more than two times with that of one occurrence.
+    Final_Formatted = re.sub(' {2,}',' ', Combined_Formatted)
+    return Final_Formatted
+
+
+CONTRACTION_MAP = {
+"ain't": "is not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he would",
+"he'd've": "he would have",
+"he'll": "he will",
+"he'll've": "he he will have",
+"he's": "he is",
+"how'd": "how did",
+"how'd'y": "how do you",
+"how'll": "how will",
+"how's": "how is",
+"i'd": "i would",
+"i'd've": "i would have",
+"i'll": "i will",
+"i'll've": "i will have",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'd've": "it would have",
+"it'll": "it will",
+"it'll've": "it will have",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"mightn't've": "might not have",
+"must've": "must have",
+"mustn't": "must not",
+"mustn't've": "must not have",
+"needn't": "need not",
+"needn't've": "need not have",
+"o'clock": "of the clock",
+"oughtn't": "ought not",
+"oughtn't've": "ought not have",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"shan't've": "shall not have",
+"she'd": "she would",
+"she'd've": "she would have",
+"she'll": "she will",
+"she'll've": "she will have",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"shouldn't've": "should not have",
+"so've": "so have",
+"so's": "so as",
+"that'd": "that would",
+"that'd've": "that would have",
+"that's": "that is",
+"there'd": "there would",
+"there'd've": "there would have",
+"there's": "there is",
+"they'd": "they would",
+"they'd've": "they would have",
+"they'll": "they will",
+"they'll've": "they will have",
+"they're": "they are",
+"they've": "they have",
+"to've": "to have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'd've": "we would have",
+"we'll": "we will",
+"we'll've": "we will have",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what'll've": "what will have",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"when's": "when is",
+"when've": "when have",
+"where'd": "where did",
+"where's": "where is",
+"where've": "where have",
+"who'll": "who will",
+"who'll've": "who will have",
+"who's": "who is",
+"who've": "who have",
+"why's": "why is",
+"why've": "why have",
+"will've": "will have",
+"won't": "will not",
+"won't've": "will not have",
+"would've": "would have",
+"wouldn't": "would not",
+"wouldn't've": "would not have",
+"y'all": "you all",
+"y'all'd": "you all would",
+"y'all'd've": "you all would have",
+"y'all're": "you all are",
+"y'all've": "you all have",
+"you'd": "you would",
+"you'd've": "you would have",
+"you'll": "you will",
+"you'll've": "you will have",
+"you're": "you are",
+"you've": "you have",
+}
+
+# The code for expanding contraction words
+def expand_contractions(text, contraction_mapping =  CONTRACTION_MAP):
+    """expand shortened words to the actual form.
+       e.g. don't to do not
+    
+       arguments:
+            input_text: "text" of type "String".
+         
+       return:
+            value: Text with expanded form of shorthened words.
+        
+       Example: 
+       Input : ain't, aren't, can't, cause, can't've
+       Output :  is not, are not, cannot, because, cannot have 
+    
+     """
+    # Tokenizing text into tokens.
+    list_Of_tokens = text.split(' ')
+
+    # Checking for whether the given token matches with the Key & replacing word with key's value.
+    
+    # Check whether Word is in lidt_Of_tokens or not.
+    for Word in list_Of_tokens: 
+        # Check whether found word is in dictionary "Contraction Map" or not as a key. 
+         if Word in CONTRACTION_MAP: 
+                # If Word is present in both dictionary & list_Of_tokens, replace that word with the key value.
+                list_Of_tokens = [item.replace(Word, CONTRACTION_MAP[Word]) for item in list_Of_tokens]
+                
+    # Converting list of tokens to String.
+    String_Of_tokens = ' '.join(str(e) for e in list_Of_tokens) 
+    return String_Of_tokens
+
+
+def removing_special_characters(text):
+    """Removing all the special characters except the one that is passed within 
+       the regex to match, as they have imp meaning in the text provided.
+   
+    
+    arguments:
+         input_text: "text" of type "String".
+         
+    return:
+        value: Text with removed special characters that don't require.
+        
+    Example: 
+    Input : Hello, K-a-j-a-l. Thi*s is $100.05 : the payment that you will recieve! (Is this okay?) 
+    Output :  Hello, Kajal. This is $100.05 : the payment that you will recieve! Is this okay?
+    
+   """
+    Formatted_Text = re.sub(r'[&]', 'and', text)
+    # The formatted text after removing not necessary punctuations.
+    Formatted_Text = re.sub(r'[^a-zA-Z0-9 .]', '', Formatted_Text)
+    Formatted_Text = re.sub(r'[?|$|!]',r'', Formatted_Text)
+    # In the above regex expression,I am providing necessary set of punctuations that are frequent in this particular dataset.
+    return Formatted_Text
+
+
+def clean_text_completely(text, tokenizer, lemmatizer):
+    
+    #clean_string = remove_whitespace(text) 
+    clean_string = accented_characters_removal(text)
+    #clean_string = lower_casing_text(clean_string)
+    clean_string = reducing_incorrect_character_repeatation(clean_string)
+    clean_string = expand_contractions(clean_string)
+    clean_string = removing_special_characters(clean_string)
+    
+    clean_string_lst = clean_string.split('.')
+    
+    return clean_string_lst   
